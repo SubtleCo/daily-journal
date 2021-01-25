6806a6692cb4a4cd263587c929f72560860c14f5
Alex Martin <alex@subtleco.co>
2021-01-25 09:11:42 -0600

    Use .map() in JournalEntryList

--- scripts/journal/JournalEntryList.js
+++ scripts/journal/JournalEntryList.js
@@ -6,10 +6,5 @@ const entryLog = document.querySelector('.containerLeft__entries');
 export const EntryList = () => {
     const entries = useJournalEntries();
 
-    let entryHTML = "";
-    for (const entry of entries) {
-        entryHTML += JournalEntryComponent(entry);
-    }
-
-    entryLog.innerHTML += entryHTML;
+    entryLog.innerHTML += entries.map(entry => JournalEntryComponent(entry));
 }
