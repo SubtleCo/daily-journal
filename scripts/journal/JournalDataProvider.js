45e74aa082858cc2700613bdc3abf732e8459c2d
Alex Martin <alex@subtleco.co>
2021-01-28 11:14:47 -0600

    Added save funcionality to journal entry, reset the form after submitting new entry

--- scripts/journal/JournalDataProvider.js
+++ scripts/journal/JournalDataProvider.js
@@ -1,3 +1,9 @@
+const eventHub = document.querySelector('#container')
+
+const dispatchStateChangeEvent = () => {
+    eventHub.dispatchEvent(new CustomEvent("journalStateChanged"))
+}
+
 let journal = []
 
 export const getJournalEntries = () => {
@@ -12,4 +18,16 @@ export const useJournalEntries = () => {
             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
     )
     return sortedByDate;
+}
+
+export const saveJournalEntry = (entryObj) => {
+    return fetch("http://localhost:8088/entries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(entryObj)
+    })
+        .then( () => getJournalEntries())
+        .then(dispatchStateChangeEvent)
 }
--- scripts/journal/JournalEntryList.js
+++ scripts/journal/JournalEntryList.js
@@ -2,12 +2,17 @@ import { getJournalEntries, useJournalEntries } from './JournalDataProvider.js';
 import { JournalEntryComponent } from './JournalEntry.js';
 
 const entryLog = document.querySelector('.containerLeft__entries');
+const eventHub = document.querySelector('#container')
 
 export const EntryList = () => {
     getJournalEntries()
         .then ( () => {
             const entries = useJournalEntries();
-            entryLog.innerHTML += entries.map(entry => JournalEntryComponent(entry)).join("");
+            entryLog.innerHTML = entries.map(entry => JournalEntryComponent(entry)).join("");
         })
 }
+
+eventHub.addEventListener("journalStateChanged", e => {
+    EntryList()
+})
 
